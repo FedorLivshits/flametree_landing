5e26383cab54c8cbc6ae12771c1c527cf2071057
Fedor Livshits <fediliv@gmail.com>
2025-06-22 20:46:55 +0300

    added main sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 console.log('Landing page ready');
+
+import { Gradient } from './components/gradient.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  new Gradient().initGradient('#hero-gradient');
+});
